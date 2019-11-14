a325b99286d134e0a423941f6fefcc0ec3689edd
Mahak <mahakrajput@gmail.com>
2019-11-15 07:53:19 +0800

    * DealLoader.cs: Load events and deals
    * EventEnricher.cs:

--- Reinsurance.FileReader/DealLoader.cs
+++ Reinsurance.FileReader/DealLoader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using Reinsurance.Model;
 using System.Collections.Generic;
 
@@ -6,14 +6,17 @@ namespace Reinsurance.FileReader
 {
     public class DealLoader
     {
-        public DealLoader()
-        {
-        }
 
         public static List<Deal> GetDeals()
         {
+            var deals = new List<Deal>
+            {
+                new Deal(1, 100, 500, new List<Peril> { Peril.Earthquake }, new List<Region> { Region.California }),
+                new Deal(2, 1000, 3000, new List<Peril> { Peril.Hurricane }, new List<Region> { Region.Florida }),
+                new Deal(3, 250, 250, new List<Peril> { Peril.Flood, Peril.Hurricane }, new List<Region> { Region.Florida, Region.Louisiana })
+            };
 
-            return new List<Deal>();
+            return deals;
         }
 
     }
--- Reinsurance.FileReader/EventEnricher.cs
+++ Reinsurance.FileReader/EventEnricher.cs
@@ -1,21 +1,49 @@
-﻿using System;
-using Reinsurance.Model;
+﻿using Reinsurance.Model;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Reinsurance.FileReader
 {
     public class EventEnricher
     {
-        public EventEnricher()
-        {
-        }
 
         public static List<Event> GetSortedAndEnrichedEvents(int[][] data)
         {
+            if(data.GetLength(0) <= 0)
+            {
+                return new List<Event>();
+            }
 
+            var events = new List<Event>();
+            Event eventResult = null;
 
+            for (int index = 0; index < data.GetLength(0); index++)
+            {
+                eventResult = GetEventFromData(data[index]);
 
-            return new List<Event>();
+                if(eventResult != null)
+                {
+                    events.Add(eventResult);
+                }
+            }
+
+            if (events.Any())
+            {
+                var sortedEvents = events.OrderBy(e => e.Id).ToList();
+                return sortedEvents;
+            }
+
+            return events;
+        }
+
+        private static Event GetEventFromData(int[] data)
+        {
+            if(data.Length != 4)
+            {
+                return null;
+            }
+
+            return new Event(data[0], (Peril)data[1], (Region)data[2], data[3]);
         }
     }
 }
